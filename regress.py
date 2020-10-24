f96ec0cbfb863760f844f607f126f35e09a6dfdc
DableUTeeF <palm22180@gmail.com>
2020-10-24 13:59:49 +0700

    Try merge

--- regress.py
+++ regress.py
@@ -8,25 +8,37 @@ import numpy as np
 def get_data():
     x = []
     y = []
-    for file in os.listdir('output/4 tiles'):
-        if 'txt' in file:
-            csv = open(os.path.join('output/4 tiles', file)).read().split('\n')
-            y.append([int(file.split('_')[0])])
-            data = []
-            for line in [csv[0], csv[2], csv[3]]:
-                l = line.split(',')
-                data.extend([int(i) for i in l])
-            x.append(data)
-    return x, y
+    for files in os.listdir('output/'):
+        if 'jpg' in files:
+            continue
+        obj = []
+        cls, no = files.split('_')
+        cls = int(cls)
+        j = cls // 6
+        i = cls % 6
+
+        y.append((i, j))
+        txt = open(os.path.join('output/', files)).read().split('\n')
+        # eye_center = txt[1]
+        # pupil_coords = txt[2]
+        # box = txt[3]
+        for line in txt:
+            if line != '':
+                a, b, c, d = line.split(',')
+                obj.extend((float(a), float(b), float(c), float(d)))
+        x.append(np.array(obj))
+    return np.array(x, dtype='float32'), y
 
 
 if __name__ == '__main__':
     x, y = get_data()
     X_train, X_test, y_train, y_test = train_test_split(x, y)
+
     model = LinearRegression()
     model.fit(X_train, y_train)
-
     y_pred = model.predict(X_test)
-    print(mean_absolute_error(y_test, y_pred))
-    for i in range(y_pred.shape[0]):
-        print(y_test[i], y_pred[i])
+    z = np.round(y_pred)
+    y_test = np.array(y_test, dtype='float64')
+    print(mean_absolute_error(z, y_test))
+    for i in range(y_test.shape[0]):
+        print(f'y_true: {y_test[i]} - y_pred: {y_pred[i]} - rounded: {z[i]}')
